af267fe6efd315901e0b1e864b55da2b5e0120d7
leobewater <mmbee888@gmail.com>
2024-07-16 11:04:17 -0400

    add background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className='absolute -z-10 inset-0'>
+      Home page
+      <Image src='/images/home.jpg' alt='car factory' fill objectFit='cover' />
+    </div>
+  );
 }
